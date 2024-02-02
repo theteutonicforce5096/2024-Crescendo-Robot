af775d1849ba8733fb66e7b5c42d78ed0cfdaffc
leo <nolanrabehl@gmail.com>
2024-02-01 19:49:45 -0600

    Did testing

--- robot.py
+++ robot.py
@@ -7,11 +7,19 @@ import math
 class MyRobot(wpilib.TimedRobot):
     def teleopInit(self) -> None:
         self.joystick=wpilib.Joystick()
+        self.timer=wpilib.Timer()
+        self.timer.restart()
         return super().teleopInit()
     
     
     def teleopPeriodic(self) -> None:
+        if self.timer.advanceIfElapsed(.5):
+            dir = self.joystick.getDirectionDegrees()
+            mag = self.joystick.getMagnitude()
+            print ("Direction is: " + dir + "    Magnitude is: " + mag)
+        
         return super().teleopPeriodic()
+    
     
 class SwerveDrive:
     def moveRobot(speed, direction):
