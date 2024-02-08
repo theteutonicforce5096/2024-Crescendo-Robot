a9666d5b58b5842fa9772e51ff0b415c65c16666
Vengeance <75193135+vengeance-bro@users.noreply.github.com>
2024-02-07 21:47:00 -0600

    commit

--- robot.py
+++ robot.py
@@ -7,24 +7,26 @@ from magicbot import MagicRobot
 from robotpy_ext.autonomous.selector import AutonomousModeSelector
 from rev import ColorSensorV3
 from photonlibpy import photonCamera
+import navx
+from navx import AHRS
 
 class MyRobot(MagicRobot):
-    # subsystems.vision.Vision()
+    #subsystems.vision.Vision()
     def createObjects(self):
         self.joystick = wpilib.Joystick(0)
         pass
     def teleopInit(self):
-        #self.colorsensor = ColorSensorV3(wpilib.I2C.Port.kOnboard)
-        #self.colorSensor = ColorSensorV3(wpilib.I2C.Port.kOnboard)
+        #self.navx = navx.AHRS.create_i2c()
+        self.colorSensor = ColorSensorV3(wpilib.I2C.Port.kMXP)
         self.cam = photonCamera.PhotonCamera('main')
 
     def teleopPeriodic(self):
-        # self.proximity = self.colorSensor.getProximity()
-        # wpilib.SmartDashboard.putNumber("Proximity", self.proximity)
-        # self.rawDetectColor = self.colorSensor.getRawColor()
-        # wpilib.SmartDashboard.putNumber("Raw Red", self.rawDetectColor.red)
-        # wpilib.SmartDashboard.putNumber("Raw Green", self.rawDetectColor.green)
-        # wpilib.SmartDashboard.putNumber("Raw Blue", self.rawDetectColor.blue)
+        self.proximity = self.colorSensor.getProximity()
+        wpilib.SmartDashboard.putNumber("Proximity", self.proximity)
+        self.rawDetectColor = self.colorSensor.getRawColor()
+        wpilib.SmartDashboard.putNumber("Raw Red", self.rawDetectColor.red)
+        wpilib.SmartDashboard.putNumber("Raw Green", self.rawDetectColor.green)
+        wpilib.SmartDashboard.putNumber("Raw Blue", self.rawDetectColor.blue)
         self.result = self.cam.getLatestResult()
         if self.result.hasTargets():
             self.targets = self.result.getTargets()
