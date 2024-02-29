5d2a20fb1f902231bf21dd4d19385e8af649395b
JayTicku <jay.ticku@gmail.com>
2024-02-29 16:58:05 -0600

    Ready for arm testing

--- arm.py
+++ arm.py
@@ -7,7 +7,7 @@ from math import pi, cos
 class Arm():
     """Class for controlling arm on robot."""
 
-    def __init__(self, left_motor_id, right_motor_id, left_motor_inverted, right_motor_inverted, encoder_id, encoder_0_position, static_gain, gravity_gain):
+    def __init__(self, left_motor_id, right_motor_id, left_motor_inverted, right_motor_inverted, encoder_id, encoder_0_position):
         """
         Constructor for Arm.
 
@@ -15,20 +15,33 @@ class Arm():
         :type right_arm_motor_id: int
         :param left_arm_motor_id: ID of left motor on Arm
         :type left_arm_motor_id: int
+        :param left_motor_inverted: Whether the left motor is inverted or not
+        :type left_motor_inverted: boolean
+        :param right_motor_inverted: Whether the left motor is inverted or not
+        :type right_motor_inverted: boolean
+        :param encoder_id: ID of encoder on Arm
+        :type encoder_id: int
+        :param encoder_0_position: Value of the Encoder when the Arm is at 0 degrees (parallel to the ground). Used as a default for Shuffleboard widget.
+        :type encoder_0_position: float
         """        
+        # Hardware initialization
         self.left_motor = phoenix6.hardware.TalonFX(left_motor_id)
         self.right_motor = phoenix6.hardware.TalonFX(right_motor_id)
         self.encoder = DutyCycleEncoder(encoder_id)
-        self.encoder_0_position = encoder_0_position
 
+        # PID Controller
         self.arm_controller = PIDController(1, 0 ,0)
         self.arm_controller.enableContinuousInput(0.0, 1.0)
-        self.static_gain = static_gain
-        self.gravity_gain = gravity_gain
+        self.static_gain = 0.05
+        self.gravity_gain = 0.25
 
+        # Encoder configs
+        self.encoder_0_position = Shuffleboard.getTab("Arm").add(f"0 Position Arm Encoder Value", encoder_0_position).withSize(2, 2).getEntry().getFloat(encoder_0_position)  
+
+        # Arm Configs
         self.arm_state = "Disabled"
         self.current_angle = None
-        self.arm_state_entry = Shuffleboard.getTab("Drivers").add(f"Arm Position", "None").withSize(2, 2).getEntry()
+        self.arm_angle_entry = Shuffleboard.getTab("Drivers").add(f"Arm Position", "None").withSize(2, 2).getEntry()
         
         self._configure_arm_motor(self.left_motor, left_motor_inverted)
         self._configure_arm_motor(self.right_motor, right_motor_inverted)
@@ -44,14 +57,16 @@ class Arm():
         motor.configurator.apply(talonfx_configs) 
 
     def reset(self):
-        self.set(0)
+        """
+        Reset the Arm. Set it to 45 degrees. 
+        """
+        self.set(45)
 
     def set(self, angle):
         """
         Sets the arm at a certain angle.
         """
-        # Maybe put in angle limit?
-        self.arm_controller.setSetpoint((angle / 360) + self.encoder_0_position)
+        self.arm_controller.setSetpoint(self.encoder_0_position + (angle / 360))
 
     def update_pid_controller(self):
         """
@@ -61,7 +76,7 @@ class Arm():
         motor_speed = self.arm_controller.calculate(encoder_position)
 
         angle = encoder_position - self.encoder_0_position
-        self.arm_state_entry.setString(f"{angle * 360}") 
+        self.arm_angle_entry.setString(f"{angle * 360}") 
 
         angle_radians = 2.0 * pi * angle
         feedforward = (self.gravity_gain * cos(angle_radians)) + self.static_gain
--- robot.py
+++ robot.py
@@ -1,199 +1,35 @@
 import wpilib
-from drivetrain import SwerveDrive
-from shooter import Shooter
-#from arm import Arm
-from color_sensor import ColorSensor
-import phoenix6
+from arm import Arm
 
 class TeutonicForceRobot(wpilib.TimedRobot):
     def robotInit(self):
         # Initialize components
-        self.drivetrain = SwerveDrive()
-        self.shooter = Shooter(40, 41, 42, False, False, False, 0.7)
-        #self.arm = Arm()
-        self.color_sensor = ColorSensor()
+        self.arm = Arm(50, 51, True, False, 0)
 
         # Initialize controllers
-        self.drivetrain_controller = wpilib.XboxController(0)
-        self.shooter_controller = wpilib.XboxController(1)
+        self.controller = wpilib.XboxController(0)
 
         # Initialize timer
         self.timer = wpilib.Timer()
-        self.drivetrain_timer = wpilib.Timer()
-        self.drop_timer = wpilib.Timer()
-        self.prime_shooter_timer = wpilib.Timer()
-        self.shoot_timer = wpilib.Timer()
-
-        self.right_motor = phoenix6.hardware.TalonFX(51)
-        self.left_motor = phoenix6.hardware.TalonFX(50)
-
-        # Set default robot speeds
-        self.forward_speed = 0
-        self.strafe_speed = 0
-        self.rotation_speed = 0
 
     def teleopInit(self):
         # Reset timers
         self.timer.restart()
-        self.drivetrain_timer.reset()
-        self.drop_timer.reset()
-        self.prime_shooter_timer.reset()
-        self.shoot_timer.reset()
 
-        # Reset robot speeds.
-        self.forward_speed = 0
-        self.strafe_speed = 0
-        self.rotation_speed = 0
-
-        # Reset Drivetrain
-        self.drivetrain.reset_drivetrain()
-        self.drivetrain.reset_gyro()
-
-        # Reset shooter
-        self.shooter.reset()
-
-        # Enable Drivetrain
-        self.drivetrain.change_drivetrain_state("Enabled")
-
-        # Enable Shooter
-        self.shooter.change_shooter_state("Idle")
+        # Reset Arm
+        self.arm.reset()
 
     def teleopPeriodic(self):
-        # Get speeds from drivetrain controller.
-        forward_speed = self.drivetrain_controller.getLeftY()
-        strafe_speed = self.drivetrain_controller.getLeftX()
-        rotation_speed = self.drivetrain_controller.getRightX()
+        if self.controller.getAButtonPressed():
+            self.arm.set(0)
+        elif self.controller.getBButtonPressed():
+            self.arm.set(30)
+        elif self.controller.getXButtonPressed():
+            self.arm.set(60)
+        elif self.controller.getYButtonPressed():
+            self.arm.set(90)
 
-        # Check if gyro needs to be reset.
-        if self.drivetrain_controller.getAButtonPressed():
-            self.drivetrain.stop_robot()
-            self.drivetrain.reset_gyro()
-            self.drivetrain_timer.restart()
-            self.drivetrain.change_drivetrain_state("Resetting Gyro")
-
-        if self.drivetrain_controller.getBButtonPressed():
-            # Forward
-            self.right_motor.set_control(phoenix6.controls.DutyCycleOut(0.25))
-            self.left_motor.set_control(phoenix6.controls.DutyCycleOut(-0.25))
-        elif self.drivetrain_controller.getXButtonPressed():
-            # Backward
-            self.right_motor.set_control(phoenix6.controls.DutyCycleOut(-0.25))
-            self.left_motor.set_control(phoenix6.controls.DutyCycleOut(0.25))
-        elif self.drivetrain_controller.getYButtonPressed():
-            self.right_motor.set_control(phoenix6.controls.DutyCycleOut(0))
-            self.left_motor.set_control(phoenix6.controls.DutyCycleOut(0))
-
-        # Check if max drivetrain speed needs to be changed.
-        if (self.drivetrain_controller.getLeftTriggerAxis() > 0.1) != (self.drivetrain_controller.getRightTriggerAxis() > 0.1):
-            self.drivetrain.change_max_drivetrain_speed(0.75)
-        elif self.drivetrain_controller.getLeftTriggerAxis() > 0.1 and self.drivetrain_controller.getRightTriggerAxis() > 0.1:
-            self.drivetrain.change_max_drivetrain_speed(1.0)
-        else:
-            self.drivetrain.change_max_drivetrain_speed(0.5)
-
-        # Set deadzone on forward speed.
-        if forward_speed > 0.1 or forward_speed < -0.1:
-            self.forward_speed = forward_speed
-        else:
-            self.forward_speed = 0
-
-        # Set deadzone on strafe speed.
-        if strafe_speed > 0.1 or strafe_speed < -0.1:
-            self.strafe_speed = strafe_speed
-        else:
-            self.strafe_speed = 0
-
-        # Set deadzone on rotation speed.
-        if rotation_speed > 0.1 or rotation_speed < -0.1:
-            self.rotation_speed = rotation_speed
-        else:
-            self.rotation_speed = 0
-
-        if self.shooter_controller.getYButtonPressed():
-            self.shooter.change_shooter_state("Reset")
-        elif self.shooter_controller.getRightBumperPressed():
-            self.shooter.change_shooter_state("Force Fire")
-        elif self.shooter_controller.getLeftBumperPressed():
-            self.shooter.change_shooter_state("Drop Everything")
-
-        match self.shooter.get_shooter_state():
-            case "Idle":
-                if self.shooter_controller.getAButtonPressed():
-                    self.shooter.start_intake_motor()
-                    self.shooter.change_shooter_state("Collecting")
-            case "Collecting":
-                if self.color_sensor.detects_ring():
-                    self.shooter.stop_intake_motor()
-                    self.shooter.change_shooter_state("Loaded")
-            case "Loaded":
-                if self.shooter_controller.getXButtonPressed():
-                    self.drivetrain.change_drivetrain_state("Disabled")
-                    self.shooter.prime_shooter()
-                    # Change Arm position
-                    self.prime_shooter_timer.restart()
-                    self.shooter.change_shooter_state("Armed")
-                elif self.shooter_controller.getBButtonPressed():
-                    self.drivetrain.change_drivetrain_state("Disabled")
-                    self.shooter.prime_shooter()
-                    # Change Arm position
-                    self.prime_shooter_timer.restart()
-                    self.shooter.change_shooter_state("Armed")
-            case "Armed":
-                if self.prime_shooter_timer.hasElapsed(2.5):
-                    self.prime_shooter_timer.reset()
-                    self.shooter.start_intake_motor()
-                    self.shoot_timer.restart()
-                    self.shooter.change_shooter_state("Fire")
-            case "Fire":
-                if self.shoot_timer.hasElapsed(0.5):
-                    self.shoot_timer.reset()
-                    self.shooter.change_shooter_state("Reset")
-            case "Force Fire":
-                self.shooter.prime_shooter()
-                self.prime_shooter_timer.restart()
-                self.shooter.change_shooter_state("Armed")
-            case "Drop Everything":
-                # Move Arm
-                self.shooter.reverse_intake_motor()
-                self.drop_timer.restart()
-                self.shooter.change_shooter_state("Stop Dropping Everything")
-            case "Stop Dropping Everything":
-                # Reset
-                if self.drop_timer.hasElapsed(1):
-                    self.shooter.change_shooter_state("Reset")
-            case "Reset":
-                self.drivetrain.change_drivetrain_state("Enabled")
-                self.shooter.reset()
-                self.shooter.change_shooter_state("Idle")
-
-        # Move robot if drivetrain is enabled.
-        match self.drivetrain.get_drivetrain_state():
-            case "Enabled":
-                if self.forward_speed != 0 or self.strafe_speed != 0 or self.rotation_speed != 0:
-                    self.drivetrain.move_robot(self.forward_speed, self.strafe_speed, self.rotation_speed) 
-                else:
-                    self.drivetrain.stop_robot()
-            case "Resetting Gyro":
-                if not self.drivetrain_timer.hasElapsed(0.5):
-                    if self.forward_speed != 0 or self.strafe_speed != 0 or self.rotation_speed != 0:
-                        self.drivetrain_controller.setRumble(wpilib.XboxController.RumbleType.kBothRumble, 0.75)  
-                    else:
-                        self.drivetrain_controller.setRumble(wpilib.XboxController.RumbleType.kBothRumble, 0)  
-                else:
-                    self.drivetrain_controller.setRumble(wpilib.XboxController.RumbleType.kBothRumble, 0)  
-                    self.drivetrain_timer.reset()
-                    self.drivetrain.change_drivetrain_state("Enabled")
-            case "Disabled":
-                if self.forward_speed != 0 or self.strafe_speed != 0 or self.rotation_speed != 0:
-                    self.drivetrain_controller.setRumble(wpilib.XboxController.RumbleType.kBothRumble, 0.75)  
-                else:
-                    self.drivetrain_controller.setRumble(wpilib.XboxController.RumbleType.kBothRumble, 0) 
-
-            #self.arm.update_pid_controller() 
-
-    def disabledInit(self):
-        # Turn off drivetrain controller rumble if it is stil on.
-        self.drivetrain_controller.setRumble(wpilib.XboxController.RumbleType.kBothRumble, 0)  
+        self.arm.update_pid_controller() 
 
 if __name__ == "__main__":
     wpilib.run(TeutonicForceRobot)
