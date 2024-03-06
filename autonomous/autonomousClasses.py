a66ece4454baaaba3fcee7089e7507bed603f2f1
leo <72452971+u-x@users.noreply.github.com>
2024-03-05 19:31:44 -0600

    Co-authored-by: Vengeance <vengeance-bro@users.noreply.github.com>

--- autonomous/autonomousClasses.py
+++ autonomous/autonomousClasses.py
@@ -1,6 +1,7 @@
 from autonomous.base_auto import BaseAuto
 from magicbot.state_machine import state, timed_state, AutonomousStateMachine
 from subsystems.drivetrain import SwerveDrive
+from subsystems.shooter import Shooter
 
 
 class Move(BaseAuto):
@@ -11,4 +12,19 @@ class Move(BaseAuto):
 
     @timed_state(duration=2, first=True)
     def moooooove(self):
+        self.drivetrain.move_robot(0.1, 0, 0)
+
+class ShootNScoot(BaseAuto):
+    MODE_NAME = "Shoot N Scoot"
+    Default = False
+
+    drivetrain = SwerveDrive()
+    shooter = Shooter()
+
+    @timed_state(duration=2, first=True, next_state="scoot")
+    def shoot(self):
+        self.shooter.release_note()
+
+    @timed_state(duration=2)
+    def scoot(self):
         self.drivetrain.move_robot(0.1, 0, 0)
